98cd0c2ec35166fc8cdde5652a5e38707cf425ae
AishaAgarwal <aishaagarwal19567@gmail.com>
2024-01-06 10:47:27 +0530

    log ingested successfully

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 import mysql.connector
 import logging
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -18,17 +19,31 @@ connection = mysql.connector.connect(
 logging.basicConfig(level=logging.INFO)
 logging.info("Connected to TiDB Cloud successfully.")
 
+class LogIngestPayload(BaseModel):
+    message: str
+    log_type: str
+
+class LogQueryPayload(BaseModel):
+    log_type: str
+
 @app.get("/")
 def read_root():
     return {"message" : "Welcome to your FastAPI application"}
 
+
+
 @app.post("/logIngest")
-def log_ingest(log_data: str):
+def log_ingest(payload: LogIngestPayload):
     try:
         cursor = connection.cursor()
+        insert_query = "INSERT INTO logs (log_type, message) VALUES (%s, %s)"
+        cursor.execute(insert_query, (payload.message, payload.log_type))
+        connection.commit()
+        logging.info("Log data ingested successfully")
         return {"message": "Log data ingested successfully"}
     
     except Exception as e:
+        logging.error(f"Error during log ingestion: {str(e)}", exc_info=True)
         raise HTTPException(status_code=500, detail="Error during log ingestion: {str(e)}")
     
     finally:
